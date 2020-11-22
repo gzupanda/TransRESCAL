dee035e1e0ef90b2a815a600769d64a553c59198
gzupanda <36719468+gzupanda@users.noreply.github.com>
2020-11-22 22:40:24 +0800

    Add files via upload

---
+++ WN18RR_h.py
@@ -0,0 +1,33 @@
+#import scipy.io
+
+import efe
+from efe.exp_generators import *
+import efe.tools as tools
+
+if __name__ =="__main__":
+
+	#Load data, ensure that data is at path: 'path'/'name'/[train|valid|test].txt
+	wn18rr = build_data(name = 'WN18RR',path = tools.cur_path + '/datasets/')
+
+
+	#SGD hyper-parameters:
+	params = Parameters(learning_rate = 0.5, 
+						max_iter = 1000, 
+						batch_size = int(len(wn18rr.train.values) / 100),  #Make 100 batches
+						neg_ratio = 1, 
+						valid_scores_every = 50,
+						learning_rate_policy = 'adagrad',
+						contiguous_sampling = False )
+
+	all_params = { "HTransRESCAL_Logistic_Model" : params } ; emb_size = 20; lmbda =0.5
+	tools.logger.info( "Learning rate: " + str(params.learning_rate))
+	tools.logger.info( "Max iter: " + str(params.max_iter))
+	tools.logger.info( "Generated negatives ratio: " + str(params.neg_ratio))
+	tools.logger.info( "Batch size: " + str(params.batch_size))
+
+
+	#Then call a local grid search, here only with one value of rank and regularization
+	wn18rr.grid_search_on_all_models(all_params, embedding_size_grid = [emb_size], lmbda_grid = [lmbda], nb_runs = 1)
+
+	#Print best averaged metrics:
+	wn18rr.print_best_MRR_and_hits()
---
+++ kinship_h.py
@@ -0,0 +1,34 @@
+#import scipy.io
+
+import efe
+from efe.exp_generators import *
+import efe.tools as tools
+
+if __name__ =="__main__":
+
+	#Load data, ensure that data is at path: 'path'/'name'/[train|valid|test].txt
+	kinship = build_data(name = 'kinship',path = tools.cur_path + '/datasets/')
+
+
+	#SGD hyper-parameters:
+	params = Parameters(learning_rate = 0.5, 
+						max_iter = 1000, 
+						batch_size = int(len(kinship.train.values) / 20),  #Make 100 batches
+						neg_ratio = 1, 
+						valid_scores_every = 50,
+						learning_rate_policy = 'adagrad',
+						contiguous_sampling = False )
+
+	
+	all_params = { "HTransRESCAL_Logistic_Model" : params } ; emb_size = 10; lmbda =0.05
+	tools.logger.info( "Learning rate: " + str(params.learning_rate))
+	tools.logger.info( "Max iter: " + str(params.max_iter))
+	tools.logger.info( "Generated negatives ratio: " + str(params.neg_ratio))
+	tools.logger.info( "Batch size: " + str(params.batch_size))
+
+
+	#Then call a local grid search, here only with one value of rank and regularization
+	kinship.grid_search_on_all_models(all_params, embedding_size_grid = [emb_size], lmbda_grid = [lmbda], nb_runs = 1)
+
+	#Print best averaged metrics:
+	kinship.print_best_MRR_and_hits()
